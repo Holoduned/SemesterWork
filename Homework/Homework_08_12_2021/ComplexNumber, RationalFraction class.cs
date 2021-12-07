0c5d26fd06067fd7017f680840897ad95eae4947
Spirina_Anastasia <holoduned@mail.ru>
2021-12-07 22:19:08 +0300

    .

---
+++ Homework/Homework_08_12_2021/ComplexNumber, RationalFraction class.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Study.Homework.Homework_08_12_2021
+{
+    public interface ICloneable
+    {
+        object Clone();
+    }
+
+    public interface IComparable
+    {
+        int CompareTo(object o);
+    }
+
+    class ComplexNumber : ICloneable
+    {
+        public double real; double imaginary; double module; double arg;
+
+        public ComplexNumber(double real_num, double imag_num)
+        {
+            real = real_num; imaginary = imag_num;
+        }
+
+        public object Clone()
+        {
+            return new ComplexNumber(real = this.real, imaginary = this.imaginary);
+        }
+
+        public ComplexNumber Addition(ComplexNumber input)
+        {
+            return new ComplexNumber(real + input.real, imaginary + input.imaginary);
+        }
+
+        public ComplexNumber Substraction(ComplexNumber input)
+        {
+            return new ComplexNumber(real - input.real, imaginary - input.imaginary);
+        }
+
+        public ComplexNumber ComplexMultiplication(ComplexNumber input)
+        {
+            return new ComplexNumber(real * input.real - imaginary * input.imaginary, real * input.imaginary + imaginary * input.real);
+        }
+
+        public ComplexNumber Multiplication(double n)
+        {
+            return new ComplexNumber(real * n, imaginary * n);
+        }
+
+        public ComplexNumber Division(ComplexNumber input)
+        {
+            return new ComplexNumber((real * input.real + imaginary * input.imaginary) / (Math.Pow(input.real, 2) + Math.Pow(input.imaginary, 2)),
+                (imaginary * input.real - real * input.imaginary) / (Math.Pow(input.real, 2) + Math.Pow(input.imaginary, 2)));
+        }
+
+        public double Length()
+        {
+            module = Math.Sqrt(real * real + imaginary * imaginary);
+            return module;
+        }
+
+        public double Arg()
+        {
+            arg = Math.Atan(imaginary / real);
+            return arg;
+        }
+
+
+        public void ToString()
+        {
+            if (real == 0 && imaginary != 0)
+            {
+                Console.WriteLine($"{imaginary}i");
+            }
+            else if (real != 0 && imaginary == 0)
+            {
+                Console.WriteLine(real);
+            }
+            else if (real == 0 && imaginary == 0)
+            {
+                Console.WriteLine(0);
+            }
+            else if (imaginary > 0)
+            {
+                Console.WriteLine($"{real} + {imaginary}i");
+            }
+            else
+            {
+                Console.WriteLine($"{real}{imaginary}i");
+            }
+        }
+
+        public bool Comparison(ComplexNumber input)
+        {
+            if (real == input.real && imaginary == input.imaginary) return true;
+            return false;
+        }
+
+        public ComplexNumber Pow(double n)
+        {
+            if (n == 0)
+            {
+                return new ComplexNumber(0, 0);
+            }
+            else
+            {
+                return new ComplexNumber(Math.Pow(module, n) * Math.Cos(n * arg), Math.Pow(module, n) * Math.Sin(n * arg));
+            }
+        }
+    }
+
+    class RationalFraction : IComparable
+    {
+        public int numenator, denumenator;
+        public int CompareTo(object o)
+        {
+            RationalFraction fraction = o as RationalFraction;
+            if (fraction != null)
+                return this.numenator.CompareTo(fraction.numenator);
+            else
+                throw new Exception("Невозможно сравнить два объекта");
+        }
+    
+
+        public RationalFraction(int num, int denum)
+        {
+            numenator = num; denumenator = denum;
+            if (denumenator == 0)
+            {
+                throw new DivideByZeroException();
+            }
+        }
+
+
+
+        public void Reduce()
+        {
+            int count = 0;
+            int a = Math.Abs(numenator); int b = Math.Abs(denumenator); int nod = 0;
+            while (a != 0 && b != 0)
+            {
+                if (a > b)
+                {
+                    a = a % b;
+                }
+                else
+                {
+                    b = b % a;
+                }
+                count++;
+            }
+            nod = a + b; numenator /= nod; denumenator /= nod;
+        }
+
+        public RationalFraction Addition(RationalFraction input)
+        {
+            if (denumenator == input.denumenator)
+            {
+                return new RationalFraction(numenator + input.numenator, denumenator);
+            }
+            else
+            {
+                return new RationalFraction(numenator * input.denumenator + input.numenator * denumenator, denumenator * input.denumenator);
+            }
+        }
+
+        public RationalFraction Subtraction(RationalFraction input)
+        {
+            if (denumenator == input.denumenator)
+            {
+                return new RationalFraction(numenator - input.numenator, denumenator);
+            }
+            else
+            {
+                return new RationalFraction(numenator * input.denumenator - input.numenator * denumenator, denumenator * input.denumenator);
+            }
+        }
+
+        public RationalFraction Multiplication(RationalFraction input)
+        {
+            return new RationalFraction(numenator * input.numenator, denumenator * input.denumenator);
+        }
+
+        public RationalFraction Division(RationalFraction input)
+        {
+            return new RationalFraction(numenator * input.denumenator, denumenator * input.numenator);
+        }
+
+
+        public void ToString()
+        {
+            if (numenator == 0)
+            {
+                Console.WriteLine("0");
+            }
+            else if (denumenator == 1)
+            {
+                Console.WriteLine(numenator);
+            }
+            else
+            {
+                Console.WriteLine($"{numenator} / {denumenator}");
+            }
+        }
+
+        public double DoubleValue()
+        {
+            return Convert.ToDouble(numenator) / denumenator;
+        }
+
+        public bool Comparison(RationalFraction input)
+        {
+            if (numenator == input.numenator && denumenator == input.denumenator) return true;
+            return false;
+        }
+
+        public int IntPart()
+        {
+            return numenator / denumenator;
+        }
+    }
+}
---
+++ Homework/Homework_08_12_2021/str extension, interfaces.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Study.Homework.Homework_08_12_2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите две строки: ");
+            string s1 = Console.ReadLine();
+            string s2 = Console.ReadLine();
+            Console.WriteLine(s1.StringAdd(s2));
+
+            Console.WriteLine("Вводите значения мнимой и действительной частей дроби");
+            var complexNumber = new ComplexNumber(Convert.ToDouble(Console.ReadLine()), Convert.ToDouble(Console.ReadLine()));
+            Console.Write("Комплексное число: "); complexNumber.ToString();
+            var copy_complexNumber = (ComplexNumber)complexNumber.Clone();
+            Console.Write("Копия комплексного числа: "); copy_complexNumber.ToString();
+            copy_complexNumber.real = 5;
+            Console.Write("Измененная копия комплексного числа и само число: "); copy_complexNumber.ToString();
+
+            Console.WriteLine("Вводите значения знаменателя и числителя дроби");
+            var fraction = new RationalFraction(Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine())); fraction.Reduce();
+            Console.Write("Первая дробь равна: "); fraction.ToString();
+            var fraction_2 = new RationalFraction(Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine())); fraction_2.Reduce();
+            Console.Write("Вторая дробь равна: "); fraction_2.ToString();
+
+            Console.WriteLine($"Результат сравнения числителей дробей: {fraction.CompareTo(fraction_2)}");
+            Console.WriteLine("Меньше 0 - число меньше. 0 - равны. Больше нуля - число больше");
+
+            Console.ReadKey();
+        }
+    }
+}
